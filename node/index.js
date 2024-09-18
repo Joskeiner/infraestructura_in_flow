d8504bc41e40262a2ed363349f9afe842a79412a
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2024-09-17 21:50:50 -0300

    documentation of the functions and add

--- node/index.js
+++ node/index.js
@@ -54,22 +54,4 @@ async function upService(choice){
 
  await main();
 
- // !node: lambda 
- // ? golang :  aurora ,rds -> por definir
- // ? python : APIGetway   -> por definir
  
- // ? documentacion 
- // https://patorjk.com/software/taag/#p=display&f=3-D&t=In%20Flow
- // https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
- // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started-nodejs.html
- // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javascriptv3/example_code/nodegetstarted/index.js
- 
- //   const choice = await prompt.question(`\t ##### Elija una de las opciones ##### 
- //         1 - Crear un bucket  de S3
- //         2 - Eliminar un bucket de S3
- //         3 - Crear una tabla de dynamoDB
- //         4 - Eliminar tabla de dynameDB
- //         5 - Eliminar lambda function
- //         6 - Subir codigo a una lanmbda
- //         \n
- //         `);

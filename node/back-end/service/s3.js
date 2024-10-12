dcf933feae778860d72718c2e08d9404801a59fd
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2024-10-12 13:55:24 -0300

    add new configuration to bucket

--- node/back-end/service/s3.js
+++ node/back-end/service/s3.js
@@ -1,127 +1,123 @@
-import { createInterface} from 'node:readline/promises'
+import { createInterface } from "node:readline/promises";
 import {
-    S3Client,
-    CreateBucketCommand, 
-    ListBucketsCommand,
-    DeleteBucketCommand,
-    paginateListObjectsV2,
-    DeleteObjectCommand,
-    PutObjectCommand
-} from "@aws-sdk/client-s3"
-
+  S3Client,
+  CreateBucketCommand,
+  ListBucketsCommand,
+  DeleteBucketCommand,
+  paginateListObjectsV2,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import "dotenv/config";
 
 // verificar si se puede configurar de forma mas detallada los buckets
 /**
  * this function create bucket s3  with name @param {string} name and timeTags
  */
 export async function createBucket(name) {
-    const s3Clinet = new S3Client({});
+  const s3Clinet = new S3Client({});
 
-    const bucketName = `${name}-${Date.now()}`;
-   let output = await s3Clinet.send(
-        new CreateBucketCommand({
-            Bucket:bucketName,
-        })
-    );
+  const bucketName = `${name}-${Date.now()}`;
+  let output = await s3Clinet.send(
+    new CreateBucketCommand({
+      Bucket: bucketName,
+      ObjectLockEnabledForBucket: process.env.OBJECT,
+    })
+  );
 
-
-    if (output.$metadata.httpStatusCode == 200 ) {
-        console.log(" se creo el bucket con exito");
-    }else {
-        console.log("error al momento de crear el bucket");
-
-    }
-
+  if (output.$metadata.httpStatusCode == 200) {
+    console.log(" se creo el bucket con exito");
+  } else {
+    console.log("error al momento de crear el bucket");
+  }
 }
 /**
- *  this function is for delete a bucket 
+ *  this function is for delete a bucket
  * @param {string} name
  */
 
 export async function DeleteBucket() {
-    const s3Client = new S3Client({});
-    let name = await ListBucket();
-    if (name == ""){
-         process.exit(2);
+  const s3Client = new S3Client({});
+  let name = await ListBucket();
+  if (name == "") {
+    process.exit(2);
+  }
+  const paginator = paginateListObjectsV2(
+    { client: s3Client },
+    { Bucket: name }
+  );
+  for await (const page of paginator) {
+    const objects = page.Contents;
+    if (objects) {
+      for (const object of objects) {
+        await s3Client.send(
+          new DeleteObjectCommand({ Bucket: name, Key: object.Key })
+        );
+      }
     }
-    const paginator = paginateListObjectsV2({client : s3Client},{Bucket: name});
-    for await (const page of paginator){
-        const objects = page.Contents;
-        if (objects){
-            for(const object of objects){
-                await s3Client.send(new DeleteObjectCommand({Bucket : name , Key: object.Key}));
-            }
-        }
-    }
-    const comand = new DeleteBucketCommand({Bucket:name})
-    let output = await s3Client.send(comand);
-    
-    if (output.$metadata.httpStatusCode == 204 ) {
-        console.log(` se elimino el bucket : ${name} con exito !!!`);
-    }
+  }
+  const comand = new DeleteBucketCommand({ Bucket: name });
+  let output = await s3Client.send(comand);
 
+  if (output.$metadata.httpStatusCode == 204) {
+    console.log(` se elimino el bucket : ${name} con exito !!!`);
+  }
 }
 
 /**
- * this function searches for the  Bucket in the list  
+ * this function searches for the  Bucket in the list
  * @returns {string}
  */
 
 async function ListBucket() {
+  let nameBucket;
+  const input = {};
+  const comand = new ListBucketsCommand(input);
+  const s3Clinet = new S3Client({});
+  const response = await s3Clinet.send(comand);
 
-    let nameBucket ;
-    const input = {};
-    const comand = new ListBucketsCommand(input);
-    const s3Clinet = new S3Client({});
-    const response = await s3Clinet.send(comand);
+  if (response.$metadata.httpStatusCode == 200) {
+    nameBucket = await choiceBucket(response.Buckets);
+  } else {
+    console.log(`error: ${response.$metadata.httpStatusCode}`);
+  }
 
-    if(response.$metadata.httpStatusCode == 200){
-         nameBucket = await choiceBucket(response.Buckets)
-    }else {
-        console.log(`error: ${response.$metadata.httpStatusCode}` );
-    }
+  if (nameBucket != "") {
+    // console.log(`nombre del bucket en listBucket ${nameBucket}`)
+    return nameBucket;
+  }
 
-    if (nameBucket != ""){
-        // console.log(`nombre del bucket en listBucket ${nameBucket}`)
-        return nameBucket;
-    }
-
-    return "";
-
+  return "";
 }
 
 /**
  * this function is used to choose a Bucket from tthe list
- * 
- * @param {import('@aws-sdk/client-s3').ListBucketsCommandOutput} buckets 
+ *
+ * @param {import('@aws-sdk/client-s3').ListBucketsCommandOutput} buckets
  * @returns {string}
  */
 async function choiceBucket(buckets) {
+  console.log("#### listado de buckets ####");
+  for (let index = 0; index < buckets.length; index++) {
+    console.log(buckets[index].Name);
+  }
 
-        
-        console.log("#### listado de buckets ####")
-    for (let index = 0; index < buckets.length; index++) {
-        
-        console.log(buckets[index].Name);
-        
-    }
+  const prompt = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-     const prompt = createInterface({
-        input: process.stdin,
-        output:process.stdout,
-    });
+  const nameBucket = await prompt.question(
+    " \n Ingrese el nombre del bucket a eliminar \n"
+  );
+  prompt.close();
 
-    const nameBucket = await prompt.question(" \n Ingrese el nombre del bucket a eliminar \n");
-    prompt.close();
+  if (nameBucket == " ") {
+    console.log(" Hubo un error al momento de procesar el nombre ");
 
-    if (nameBucket == " "){
-        console.log(" Hubo un error al momento de procesar el nombre ")
+    return "";
+  }
 
-        return "";
-    }
-
-    return nameBucket.trim();
-
+  return nameBucket.trim();
 }
 
 // https://patorjk.com/software/taag/#p=display&f=3-D&t=In%20Flow
